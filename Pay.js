3c96be274db65556d7b6485bef926efb671c56aa
Jonas <57268401+gueiros9@users.noreply.github.com>
2019-11-21 12:28:53 -0300

    Update Pay.js

--- Pay.js
+++ Pay.js
@@ -1,162 +1,195 @@
-<!DOCTYPE >
+<!DOCTYPE html>
 <html>
     <head>
-        <meta http-equiv="content-type" content="text/html; charset=UTF-8"></meta>
         <title>Google Pay</title>
-        
-        <meta http-equiv="content-type" content="text/html; charset=UTF-8"></meta>
-        <meta name="robots" content="noindex, nofollow"></meta>
-        <meta name="googlebot" content="noindex, nofollow"></meta>
-        <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
-        
         <script
          type="text/javascript"
          src="/js/lib/dummy.js"    
         ></script>
         
-        <link rel="stylesheet" type="text/css" href="/css/result-light.css"></link>
+        <link rel="stylesheet" type="text/css" href="/css/result-light.css">
         <style id="compiled-css" type="text/css"></style>
 
         <script type="text/javascript"
-         const baseRequest = {
-             apiVersion: 2,
-             apiVersionMinor: 0
-            };
+          const baseRequest = {
+   apiVersion: 2,
+   apiVersionMinor: 0
+ };
 
-         const allowedCardNetworks = ["AMEX", "DISCOVER", "INTERAC", "JCB", "MASTERCARD", "VISA"];
-         const allowedCardAuthMethods = ["PAN_ONLY", "CRYPTOGRAM_3DS"];
-        
-         /** const tokenizationSpecification = {
-             "type": "DIRECT",
-             "parameters": {
-                 "protocolVersion": "ECv2",
-                 "publicKey": "AIzaSyCRq4xiasSjvfLYwj7B_o-MWZqHL3082gY"
-                }
-            }
-         */
-        
-         const tokenizationSpecification = {
-         type: 'PAYMENT_GATEWAY',
-         parameters: {
-                 'gateway': 'example',
-                 'gatewayMerchantId': 'exampleGatewayMerchantId'
-                }   
-            }
+ const allowedCardNetworks = ["AMEX", "DISCOVER", "INTERAC", "JCB", "MASTERCARD", "VISA"];
+ const allowedCardAuthMethods = ["PAN_ONLY", "CRYPTOGRAM_3DS"];
 
-         const baseCardPaymentMethod = {
-             type: 'CARD',
-             parameters: {
-                 allowedAuthMethods: allowedCardAuthMethods,
-                 allowedCardNetworks: allowedCardNetworks
-                }
-            };
+ /** const tokenizationSpecification = {
+     "type": "DIRECT",
+     "parameters": {
+         "protocolVersion": "ECv2",
+         "publicKey": "AIzaSyCRq4xiasSjvfLYwj7B_o-MWZqHL3082gY"
+        }
+    }
+ */
 
-         const cardPaymentMethod = Object.assign(
-             {},
-             baseCardPaymentMethod,
-             {
-                 tokenizationSpecification: tokenizationSpecification
-             }
-            
-            );
-         let paymentsClient = null;
-         function getGoogleIsReadyToPayRequest() {
-             return Object.assign(
-                 {},
-                 baseRequest,
-                 {
-                     allowedPaymentMethods: [baseCardPaymentMethod]
-                 }
-                );
-            }
+ const tokenizationSpecification = {
+   type: 'PAYMENT_GATEWAY',
+   parameters: {
+     'gateway': 'example',
+     'gatewayMerchantId': 'exampleGatewayMerchantId'
+   }
+ }
 
-         function getGooglePaymentDataRequest() {
-             const paymentDataRequest = Object.assign({}, baseRequest);
-             paymentDataRequest.allowedPaymentMethods = [cardPaymentMethod];
-             paymentDataRequest.transactionInfo = getGoogleTransactionInfo();
-             paymentDataRequest.merchantInfo = {
-                 // @todo a merchant ID is available for a production environment after approval by Google
-                 // See {@link https://developers.google.com/pay/api/web/guides/test-and-deploy/integration-checklist|Integration checklist}
-                 // merchantId: '01234567890123456789',
-                 merchantName: 'Example Merchant'
-                } ;
-             return paymentDataRequest;
-            }
+ const baseCardPaymentMethod = {
+   type: 'CARD',
+   parameters: {
+     allowedAuthMethods: allowedCardAuthMethods,
+     allowedCardNetworks: allowedCardNetworks
+   }
+ };
 
-         function getGooglePaymentsClient() {
-             if ( paymentsClient === null ) {
-                 paymentsClient = new google.payments.api.PaymentsClient({environment: 'TEST'});
-                }
-             return paymentsClient;
-            }
+ const cardPaymentMethod = Object.assign({},
+   baseCardPaymentMethod, {
+     tokenizationSpecification: tokenizationSpecification
+   }
 
-         function onGooglePayLoaded() {
-             const paymentsClient = getGooglePaymentsClient();
-             paymentsClient.isReadyToPay(getGoogleIsReadyToPayRequest())
-             .then(function(response) {
-             if (response.result) {
-                 addGooglePayButton();
-                 // @todo prefetch payment data to improve performance after confirming site functionality
-                 // prefetchGooglePaymentData();
-                }
-            })
-             .catch(function(err) {
-                 // show error in developer console for debugging
-                 console.error(err);
-                });
-            }
+ );
+ let paymentsClient = null;
 
-         function addGooglePayButton() {
-             const paymentsClient = getGooglePaymentsClient();
-             const button =
-             paymentsClient.createButton({onClick: onGooglePaymentButtonClicked});
-             document.getElementById('container').appendChild(button);
-            }
+ function getGoogleIsReadyToPayRequest() {
+   return Object.assign({},
+     baseRequest, {
+       allowedPaymentMethods: [baseCardPaymentMethod]
+     }
+   );
+ }
 
-         function getGoogleTransactionInfo() {
-             return {
-                 /** adicionar taxas e direcionar para o shopdin
-                 * 12% total 
-                 * 2% ong
-                 * 4% cashback
-                 * 6% shopdin
-                 */                  
-                 currencyCode: 'BRL',
-                 totalPriceStatus: 'FINAL',
-                 // set to cart total
-                 totalPrice: '1.00'
-                };
-            }
+function getGooglePaymentDataRequest() {
+  const paymentDataRequest = Object.assign({}, baseRequest);
+  paymentDataRequest.allowedPaymentMethods = [cardPaymentMethod];
+  paymentDataRequest.transactionInfo = getGoogleTransactionInfo();
+  paymentDataRequest.merchantInfo = {
+    // @todo a merchant ID is available for a production environment after approval by Google
+    // See {@link https://developers.google.com/pay/api/web/guides/test-and-deploy/integration-checklist|Integration checklist}
+    // merchantId: '01234567890123456789',
+    merchantName: 'Example Merchant'
+  };
 
-         function prefetchGooglePaymentData() {
-             const paymentDataRequest = getGooglePaymentDataRequest();
-             // transactionInfo must be set but does not affect cache
-             paymentDataRequest.transactionInfo = {
-                 totalPriceStatus: 'NOT_CURRENTLY_KNOWN',
-                 currencyCode: 'BRL'
-                };
-             const paymentsClient = getGooglePaymentsClient();
-             paymentsClient.prefetchPaymentData(paymentDataRequest);
-            }
+  paymentDataRequest.callbackIntents = ["PAYMENT_AUTHORIZATION"];
 
-         function onGooglePaymentButtonClicked() {
-             const paymentDataRequest = getGooglePaymentDataRequest();
-             paymentDataRequest.transactionInfo = getGoogleTransactionInfo();
-             const paymentsClient = getGooglePaymentsClient();
-             paymentsClient.loadPaymentData(paymentDataRequest)
-             .then(function(paymentData) {
-                 // handle the response
-                 processPayment(paymentData);
-                })
-             .catch(function(err) {
-                 // show error in developer console for debugging
-                 console.error(err);
-                });
-            }
+  return paymentDataRequest;
+}
 
-         function processPayment(paymentData) {
-             paymentToken = paymentData.paymentMethodData.tokenizationData.token;
-            }
+/**
+
+*/
+function getGooglePaymentsClient() {
+  if ( paymentsClient === null ) {
+    paymentsClient = new google.payments.api.PaymentsClient({
+        environment: 'TEST',
+      paymentDataCallbacks: {
+        onPaymentAuthorized: onPaymentAuthorized
+      }
+    });
+  }
+  return paymentsClient;
+}
+
+/**
+
+*/
+function onPaymentAuthorized(paymentData) {
+        return new Promise(function(resolve, reject){
+    // handle the response
+    processPayment(paymentData)
+    .then(function() {
+      resolve({transactionState: 'SUCCESS'});
+    })
+    .catch(function() {
+      resolve({
+        transactionState: 'ERROR',
+        error: {
+          intent: 'PAYMENT_AUTHORIZATION',
+          message: 'Insufficient funds',
+          reason: 'PAYMENT_DATA_INVALID'
+        }
+      });
+        });
+  });
+}
+
+/**
+
+*/
+function onGooglePayLoaded() {
+  const paymentsClient = getGooglePaymentsClient();
+  paymentsClient.isReadyToPay(getGoogleIsReadyToPayRequest())
+      .then(function(response) {
+        if (response.result) {
+          addGooglePayButton();
+        }
+      })
+      .catch(function(err) {
+        // show error in developer console for debugging
+        console.error(err);
+      });
+}
+
+/**
+
+*/
+function addGooglePayButton() {
+  const paymentsClient = getGooglePaymentsClient();
+  const button =
+      paymentsClient.createButton({onClick: onGooglePaymentButtonClicked});
+  document.getElementById('container').appendChild(button);
+}
+
+/**
+
+ */
+function getGoogleTransactionInfo() {
+  return {
+        displayItems: [
+        {
+          label: "Subtotal",
+          type: "SUBTOTAL",
+          price: "11.00",
+        },
+      {
+          label: "Tax",
+          type: "TAX",
+          price: "1.00",
+        }
+    ],
+    currencyCode: "BRL",
+    totalPriceStatus: "FINAL",
+    totalPrice: "990000098",
+    totalPriceLabel: "Total"
+  };
+}
+
+/**
+ * Show Google Pay payment sheet when Google Pay payment button is clicked
+ */
+function onGooglePaymentButtonClicked() {
+  const paymentDataRequest = getGooglePaymentDataRequest();
+  paymentDataRequest.transactionInfo = getGoogleTransactionInfo();
+
+  const paymentsClient = getGooglePaymentsClient();
+  paymentsClient.loadPaymentData(paymentDataRequest);
+}
+
+/**
+
+ */
+function processPayment(paymentData) {
+        return new Promise(function(resolve, reject) {
+        setTimeout(function() {
+                // @todo pass payment token to your gateway to process payment
+                paymentToken = paymentData.paymentMethodData.tokenizationData.token;
+
+        resolve({});
+    }, 3000);
+  });
+}
+
         /script>
     </head>
 
